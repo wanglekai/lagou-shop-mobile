f80454dcc255ff66f01d3b460755faa974c2bbfe
wanglekai <xiaoguailele@foxmail.com>
2021-09-13 00:11:01 +0800

    add home-header

---
+++ src/views/Home/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <img height="30" src="@/assets/logo.png" alt="logo" />
+        <div class="search">
+            <van-icon name="search" size=".6rem" />
+        </div>
+        <van-icon name="wap-nav" size=".8rem" />
+    </header>
+</template>
+<style scoped lang="scss">
+header {
+    position: fixed;
+    top: 0;
+    width: 100%;
+    display: flex;
+    height: 50px;
+    align-items: center;
+    background-color: #fff;
+    .search {
+        width: 100%;
+        height: 30px;
+        padding-left: 5px;
+        background-color: #e4e4e4;
+        border-radius: 25px;
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,9 +1,10 @@
 <template>
-  Home
+  <home-header />
 
   <layout-footer />
 </template>
 
 <script setup>
+import HomeHeader from './components/Header.vue'
 import LayoutFooter from '@/components/LayoutFooter.vue'
 </script>
