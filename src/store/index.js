56750839c2235f25164a76a68d2cfb8a6d0514a0
wanglekai <xiaoguailele@foxmail.com>
2021-09-23 22:12:21 +0800

    cart-checked-cart_num
    vuex 中对 购物车的选择状态进行管理，
    对修改购物车数量进行异步操作的管理

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,6 @@
+import { Toast } from 'vant'
 import { createStore } from 'vuex'
+import { changeCartItemNum } from '../api/cart'
 
 export default createStore({
     state () {
@@ -19,6 +21,35 @@ export default createStore({
         },
         clearCart (state) {
             state.cartList = []
+        },
+        changeChecked (state, { id, checked }) {
+            const currentIitem = state.cartList.find(item => item.id === id)
+
+            if (currentIitem) {
+                currentIitem.checked = checked
+            } else {
+                Toast.fail('选中状态异常')
+            }
+        },
+        changeCartNum (state, { id,  cart_num}) {
+            const currentIitem = state.cartList.find(item => item.id === id)
+
+            if (currentIitem) {
+                currentIitem.cart_num = cart_num
+            } else {
+                Toast.fail('修改购物车数量异常')
+            }
+        }
+    },
+    actions: {
+        async changeCartNum({ commit }, {id, cart_num}) {
+
+            commit('changeCartNum', { id, cart_num })
+
+            const { data } = await changeCartItemNum({ id, number: cart_num })
+
+            if (data.status !== 200) return Toast.fail('修改购物车数量异常: ', data.msg)
+
         }
     }
 })
--- src/views/Cart/CartIitem.vue
+++ src/views/Cart/CartIitem.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="cart-item">
-        <van-checkbox class="check-box" v-model="cart.checked" checked-color="#ee0a24" />
+        <van-checkbox class="check-box" v-model="itemChecked" checked-color="#ee0a24" />
         <div class="cart-info"  @click="toDetail">
             <img :src="cart.image" alt="">
             <div class="cart-detail">
@@ -9,7 +9,7 @@
                     <span class="price">￥ {{ currentPrice }}</span>
                     <van-stepper
                         @click.stop
-                        v-model="cart.cart_num" 
+                        v-model="cartNum" 
                         :max="cart.stock" 
                         button-size="24" />
                 </div>
@@ -23,6 +23,7 @@
 </template>
 
 <script>
+import store from '../../store'
 // import { computed, reactive } from "@vue/reactivity";
 export default {
     props: {
@@ -35,6 +36,30 @@ export default {
         currentPrice () {
             let res = parseFloat(this.cart.price) * this.cart.cart_num
             return parseFloat(res.toFixed(2))
+        },
+        // 选择状态 
+        itemChecked: {
+            get () {
+                 return this.cart.checked
+            },
+            set (value) {
+                store.commit('changeChecked', {
+                    id: this.cart.id,
+                    checked: value
+                })
+            }
+        },
+        // 购物车数量
+        cartNum: {
+            get () {
+                return this.cart.cart_num
+            },
+            set (value) {
+                store.dispatch('changeCartNum', {
+                    id: this.cart.id,
+                    cart_num: value
+                })
+            }
         }
     },
     methods: {
